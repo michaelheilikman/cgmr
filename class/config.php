6a4d2723d9cc535a009c0bb4b7f463c78def26dd
Michael Heilikman <michael.heilikman@ifip.asso.fr>
2024-01-07 01:51:05 +0100

    fix : variables d'environnement pour le LOCAL

--- class/config.php
+++ class/config.php
@@ -10,18 +10,19 @@ $company = "cgmr";
 $liveURL = "https://cgmr.fr";
 $noCacheFile = strtotime(Date('Y-m-d h:i:s'));
 
+require __DIR__ . '/../vendor/autoload.php';
 
 if($_SERVER['HTTP_HOST'] == "localhost"){
-    $host 		= "localhost";
-    $login		= "root";
-	$pass 		= "root";
-    $bdd		= "cgmr";
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+    $dotenv->load();
+    $host 		= $_ENV['DB_HOST_LOCAL'];
+	$login		= $_ENV['DB_USER_LOCAL'];
+	$pass 		= $_ENV['DB_PASSWORD_LOCAL'];
+    $bdd		= $_ENV['DB_NAME_LOCAL'];
     $path       = "http://".$_SERVER['SERVER_NAME']."/".$website."/";
 }else{
-    require __DIR__ . '/../vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
     $dotenv->load();
-
     $host 		= $_ENV['DB_HOST'];
 	$login		= $_ENV['DB_USER'];
 	$pass 		= $_ENV['DB_PASSWORD'];
